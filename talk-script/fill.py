d6a03cf2af2fa9a3e0f8b6735451081c0b7ba8a0
kzk-maeda <kzk.maeda0711@gmail.com>
2022-08-25 22:56:00 +0900

    fill

---
+++ talk-script/fill.py
@@ -0,0 +1,31 @@
+import csv
+from fileinput import filename
+import shutil
+
+
+def replace(file, row):
+    with open(file, encoding="utf-8") as f:
+        data_lines = f.read()
+    data_lines = data_lines.replace("${id}", row['id'])
+    data_lines = data_lines.replace("${title}", row['title'])
+    data_lines = data_lines.replace("${name}", row['name'])
+    data_lines = data_lines.replace("${track}", row['track'])
+    data_lines = data_lines.replace("${company}", row['company'])
+    data_lines = data_lines.replace("${starttime}", row['starttime'])
+    data_lines = data_lines.replace("${nexttime}", row['nexttime'])
+
+    with open(file, mode="w", encoding="utf-8") as f:
+        f.write(data_lines)
+
+
+if __name__ == "__main__":
+    with open('data.csv') as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            print(row)
+            file_name = f"inter-session-speeches-{row['id']}.md"
+            shutil.copyfile("inter-session-speeches-template.md", file_name)
+            replace(file_name, row)
+
+
+
